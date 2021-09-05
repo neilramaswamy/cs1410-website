d73abb43f0a3cc915ad5f7283f35e066fdbba69f
rjiang6 <renajiang@renas-mbp.devices.brown.edu>
2021-09-05 12:50:13 -0400

    added homebg

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,28 @@
 import { Header } from 'components/Header'
-import styles from 'styles/Home.module.scss'
+import 'styles/Home.module.scss'
+import bg from '../img/homebg.png'
 
 const Home = (): JSX.Element => {
-    const leftColumn = (
-        <div className={`${styles.textContainer} ${styles.column}`}>
+
+    return (
+        <div className='home'>
+            <Header />
+
+            {/* <div className={styles.content}>{leftColumn}</div> */}
+
             <h1>Welcome to CS 1410.</h1>
 
             <p>
                 We're gonna teach you how to teach computers common sense. All while trying to be
-                ethical about it.
+                ethical about it. jdiaof
             </p>
-        </div>
-    )
 
-    return (
-        <div className={styles.container}>
-            <Header />
+            <div className='test'/>
 
-            <div className={styles.content}>{leftColumn}</div>
+            <img src={bg} alt='home background image'/>
+
+            <div className='home-bg'/>
+
         </div>
     )
 }
